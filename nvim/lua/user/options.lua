3640fb4551ed6931b514fc3ff91ec23833647802
Sokratis Vasiliou <sokpanvas@gmail.com>
2022-09-27 15:16:51 +0300

    basic options and colorscheme

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,15 @@
+local set = vim.opt
+set.number = true
+set.relativenumber = true
+
+set.tabstop = 4 
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+set.smartindent = true
+set.mouse = 'a'
+set.scrolloff = 10 
+
+set.autoindent = true
+set.fileencoding = 'utf-8'
+set.termguicolors = true
